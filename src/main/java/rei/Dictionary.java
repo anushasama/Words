8bf72668fed23e889db2d4112a4183965a5317d9
AnushaSama <anu.reddy8055@gmail.com>
2019-02-23 12:45:39 -0800

    Dictionary class

---
+++ src/main/java/rei/Dictionary.java
@@ -0,0 +1,38 @@
+package rei;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Dictionary {
+	
+	static boolean isEnglishWord(String word) throws IOException {
+			 
+		Boolean status = false;
+		
+		String dictionaryURL = "https://docs.oracle.com/javase/tutorial/collections/interfaces/examples/dictionary.txt";
+		
+		URL dictionary = new URL(dictionaryURL);
+		URLConnection dictCon = dictionary.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(dictCon.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+        	
+        	if ( inputLine.equals(word)) {
+        		System.out.println(inputLine + "  " + word);
+        		status = true;
+        		break;
+        	}
+        }
+       in.close();
+			
+       return status;
+			
+	}
+	
+	
+	
+}
--- src/main/java/rei/FindingWords.java
+++ src/main/java/rei/FindingWords.java
@@ -1,5 +1,25 @@
 package rei;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FindingWords {
 
+	public static void main(String[] args) throws IOException {
+		
+		 Scanner in = new Scanner(System.in);
+		 System.out.println("Enter a String: ");
+		 String input = in.nextLine();
+		  
+		  Boolean realWord = Dictionary.isEnglishWord(input);
+		  if(realWord) {
+			  System.out.println("Given String exists in dictionary");
+		  } else {
+			  System.out.println("Given String doesn't exists in dictionary");
+		  }
+		  
+		 //Dictionary.isEnglishWord(input);
+	
+	}
+	
 }
